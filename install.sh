353c59a8b92bd0862de24cc68b6728abbc21c7c9
Pezhman Hajipour <137982619+Pezhman5252@users.noreply.github.com>
2024-05-08 15:46:35 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # نصب کتابخانه‌های مورد نیاز
-pip install random httpx os time requests
+# pip install random httpx os time requests
+pip install httpx requests
+
 
 # اجرای کد پایتون
 python3 main.py

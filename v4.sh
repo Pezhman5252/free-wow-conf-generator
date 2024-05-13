8d4737ef090de45ca7665a6e9193945977eef4bb
Pezhman Hajipour <137982619+Pezhman5252@users.noreply.github.com>
2024-05-13 20:32:08 +0330

    Update v4.sh

--- v4.sh
+++ v4.sh
@@ -212,7 +212,7 @@ endipresult() {
 	freeCloudflareAccount
 	freeCloudflareAccount2
 
-template='$Endip_v4_ip'
+template='ddddddd'
 	# echo "$template"
  	# Print the template in green
   	echo -e "${green}$template${rest}"
